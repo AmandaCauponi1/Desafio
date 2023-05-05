881478fc96c84183498cad0217700bad38058d3c
AmandaCauponi1 <127330944+AmandaCauponi1@users.noreply.github.com>
2023-05-05 17:25:06 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,62 @@
+document.querySelector('.mon').addEventListener('mouseover', () =>{
+    document.querySelector('.mon').style.background = 'hsl(186, 34%, 60%)';
+
+    document.querySelector('.mon').style.background = 'hsl(186, 34%, 60%)';
+})
+
+document.querySelector('.tue').addEventListener('mouseover', () =>{
+    document.querySelector('.tue').style.background = 'hsl(186, 34%, 60%)';
+})
+
+document.querySelector('.wed').addEventListener('mouseover', () =>{
+    document.querySelector('.wed').style.background = 'hsl(186, 34%, 60%)';
+})
+
+document.querySelector('.thu').addEventListener('mouseover', () =>{
+    document.querySelector('.thu').style.background = 'hsl(186, 34%, 60%)';
+})
+
+document.querySelector('.fri').addEventListener('mouseover', () =>{
+    document.querySelector('.fri').style.background = 'hsl(186, 34%, 60%)';
+})
+
+document.querySelector('.sat').addEventListener('mouseover', () =>{
+    document.querySelector('.sat').style.background = 'hsl(186, 34%, 60%)';
+})
+
+document.querySelector('.sun').addEventListener('mouseover', () =>{
+    document.querySelector('.sun').style.background = 'hsl(186, 34%, 60%)';
+})
+
+
+
+
+
+document.querySelector('.mon').addEventListener('mouseout', () =>{
+    document.querySelector('.mon').style.background = ' hsl(10, 79%, 65%)';
+})
+
+document.querySelector('.tue').addEventListener('mouseout', () =>{
+    document.querySelector('.tue').style.background = ' hsl(10, 79%, 65%)';
+})
+
+document.querySelector('.wed').addEventListener('mouseout', () =>{
+    document.querySelector('.wed').style.background = ' hsl(10, 79%, 65%)';
+})
+
+document.querySelector('.thu').addEventListener('mouseout', () =>{
+    document.querySelector('.thu').style.background = ' hsl(10, 79%, 65%)';
+})
+
+document.querySelector('.fri').addEventListener('mouseout', () =>{
+    document.querySelector('.fri').style.background = ' hsl(10, 79%, 65%)';
+})
+
+document.querySelector('.sat').addEventListener('mouseout', () =>{
+    document.querySelector('.sat').style.background = ' hsl(10, 79%, 65%)';
+})
+
+document.querySelector('.sun').addEventListener('mouseout', () =>{
+    document.querySelector('.sun').style.background = ' hsl(10, 79%, 65%)';
+})
+
